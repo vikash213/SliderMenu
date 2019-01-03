77cc153849361d9be6bcdf7ecaae993edecd3ba3
Appinventiv <appinventiv@Vikash-MBP.localdomain>
2019-01-03 11:45:26 +0530

    Files updated

--- SliderMenu/ContainerViewController.swift
+++ SliderMenu/ContainerViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ContainerViewController: UIViewController {
+class ContainerViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,14 +19,15 @@ class ContainerViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tableCell")
+        return cell ?? UITableViewCell()
+       // return UITableViewCell()
+    }
 
 }
--- SliderMenu/ViewController.swift
+++ SliderMenu/ViewController.swift
@@ -6,15 +6,51 @@
 //  Copyright © 2019 Vikash. All rights reserved.
 //
 
+protocol NotifySliderView {
+    func notifyView()
+}
+
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var wcLabel: UILabel!
+    
+    var isNavigationShown: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
 
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.containerView.center.x += self.view.bounds.width
+    }
+    
+    @IBAction func clickMenuButton(_ sender: UIButton) {
+        UIView.animate(withDuration: 1.0) {
+            if self.isNavigationShown {
+                //self.wcLabel.isHidden = true
+                self.containerView.center.x += self.view.bounds.width
+                self.wcLabel.alpha = 0.0
+               // self.topView.center.x = self.view.center.x + self.containerView.center.x
+               // self.topView.backgroundColor = UIColor.black
+            } else {
+                //self.wcLabel.isHidden = false
+                self.containerView.center.x -= self.view.bounds.width
+                //self.topView.center.x  = self.view.center.x
+                self.wcLabel.alpha = 1.0
+                //self.topView.backgroundColor = UIColor.white
+                
+            }
+            self.isNavigationShown = !self.isNavigationShown
+        }
+    }
+    
 
 }
 
